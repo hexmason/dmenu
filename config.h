bb0b969d45a4b2abddf935de9faecf749a09dfe5
hexmason <hexmason@proton.me>
2025-09-10 19:34:14 +0300

    center patch applied

--- config.h
+++ config.h
@@ -2,6 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 static const int vertpad = 10;              /* vertical padding of bar */
 static const int sidepad = 15;              /* horizontal padding of bar */
 
