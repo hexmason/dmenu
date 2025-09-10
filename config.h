0baead237ff8935f8b0372b1d805c56b4a8c43f3
hexmason <hexmason@proton.me>
2025-09-10 22:03:25 +0300

    noinput patch applied

--- config.h
+++ config.h
@@ -2,15 +2,15 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int draw_input = 1;                  /* -noi option; if 0, the input will not be drawn by default */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+static int min_width = 1000;                    /* minimum width when centered */
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
 static const int vertpad = 10;              /* vertical padding of bar */
 static const int sidepad = 15;              /* horizontal padding of bar */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    // "JetBrainsMono Nerd Font:size=14:antialias=true:autohint=true"
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
